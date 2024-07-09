c08d83bbf0b0173a045b59711098eb449a049e4f
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-07-09 14:10:54 +0100

    modify

--- .config/nvim/lua/iani/plugins/gitsigns.lua
+++ .config/nvim/lua/iani/plugins/gitsigns.lua
@@ -1,80 +1,82 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    require("gitsigns").setup({
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
-      signcolumn = true,
-      attach_to_untracked = true,
-      watch_gitdir = {
-        interval = 700,
-        follow_files = true,
-      },
-      current_line_blame = true,
-      current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-      current_line_blame_opts = {
-        virt_text = true,
-        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-        delay = 700,
-        ignore_whitespace = false,
-      },
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "+" },
+					change = { text = "~" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+				},
+				signcolumn = true,
+				attach_to_untracked = true,
+				watch_gitdir = {
+					interval = 700,
+					follow_files = true,
+				},
+				current_line_blame = true,
+				current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+					delay = 700,
+					ignore_whitespace = false,
+				},
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
 
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
 
-        -- Navigation
-        map("n", "]c", function()
-          if vim.wo.diff then
-            return "]c"
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true })
+					-- Navigation
+					map("n", "]c", function()
+						if vim.wo.diff then
+							return "]c"
+						end
+						vim.schedule(function()
+							gs.next_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
 
-        map("n", "[c", function()
-          if vim.wo.diff then
-            return "[c"
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true })
+					map("n", "[c", function()
+						if vim.wo.diff then
+							return "[c"
+						end
+						vim.schedule(function()
+							gs.prev_hunk()
+						end)
+						return "<Ignore>"
+					end, { expr = true })
 
-        -- Actions
-        map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
-        map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
-        map("n", "<leader>hS", gs.stage_buffer, { desc = "stage buffer" })
-        map("n", "<leader>ha", gs.stage_hunk, { desc = "stage hunk" })
-        map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "undo stage hunk" })
-        map("n", "<leader>hR", gs.reset_buffer, { desc = "reset buffer" })
-        map("n", "<leader>hp", gs.preview_hunk, { desc = "preview hunk" })
-        map("n", "<leader>hb", function()
-          gs.blame_line({ full = true })
-        end, { desc = "blame line" })
-        map("n", "<leader>tb", gs.toggle_current_line_blame)
-        map("n", "<leader>hd", gs.diffthis, { desc = "diffthis" })
-        map("n", "<leader>hD", function()
-          gs.diffthis("~")
-        end, { desc = "fn diffthis" })
-        map("n", "<leader>td", gs.toggle_deleted, { desc = "Gitsigns toggle deleted" })
+					-- Actions
+					map({ "n", "v" }, "<leader>hs", ":Gitsigns stage_hunk<CR>")
+					map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>")
+					map("n", "<leader>hS", gs.stage_buffer, { desc = "stage buffer" })
+					map("n", "<leader>ha", gs.stage_hunk, { desc = "stage hunk" })
+					map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "undo stage hunk" })
+					map("n", "<leader>hR", gs.reset_buffer, { desc = "reset buffer" })
+					map("n", "<leader>hp", gs.preview_hunk, { desc = "preview hunk" })
+					map("n", "<leader>hb", function()
+						gs.blame_line({ full = true })
+					end, { desc = "blame line" })
+					map("n", "<leader>tb", gs.toggle_current_line_blame)
+					map("n", "<leader>hd", gs.diffthis, { desc = "diffthis" })
+					map("n", "<leader>hD", function()
+						gs.diffthis("~")
+					end, { desc = "fn diffthis" })
+					map("n", "<leader>td", gs.toggle_deleted, { desc = "Gitsigns toggle deleted" })
 
-        -- Text object
-        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
-      end,
-    })
-  end,
+					-- Text object
+					map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+				end,
+			})
+		end,
+	},
 }
--- .config/nvim/lua/iani/plugins/init.lua
+++ .config/nvim/lua/iani/plugins/init.lua
@@ -1,11 +1,27 @@
 return {
-  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
 
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
 
-  "inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
+	"inkarkat/vim-ReplaceWithRegister", -- replace with register contents using motion (gr + motion)
 
-  "tpope/vim-fugitive",
+	"tpope/vim-fugitive",
 
-  "ThePrimeagen/git-worktree.nvim",
+	"ThePrimeagen/git-worktree.nvim",
+	{
+		"mistricky/codesnap.nvim",
+		build = "make",
+		cmd = "CodeSnapPreviewOn",
+		opts = {
+			watermark = nil,
+		},
+	},
+	{
+		"LudoPinelli/comment-box.nvim",
+		lazy = false,
+		keys = {
+			{ "<leader>cb", "<cmd>lua require('comment-box').llbox()<CR>", desc = "comment box" },
+			{ "<leader>cb", "<cmd>lua require('comment-box').llbox()<CR>", mode = "v", desc = "comment box" },
+		},
+	},
 }
